d66aa4de80cf3ac06baaa2d8d7ce1a41a0f09350
akilhylton <hyltonakil@gmail.com>
2021-08-18 12:02:19 -0400

    Programmer can now be changed in the flash_mjolnir script

--- firmware/flash_mjolnir.sh
+++ firmware/flash_mjolnir.sh
@@ -2,10 +2,12 @@
 
 MN_REPO=https://github.com/micronucleus/micronucleus.git
 T85=t85_default
+PROGRAMMER=atmelice_isp # CHANGE IF NEEDED.
 
 if [ ! -d /micronucleus ]; then
 	git clone $MN_REPO
 fi
 
-cd micronucleus/firmware && make config=$t85 fuse && make config=$t85 flash && rm -rf micronucleus/
+cd micronucleus/firmware && make config=$t85 fuse && rm -rf micronucleus/
 
+avrdude -c $PROGRAMMER -p t85 -U flash:r:t85_default.hex:i -B 20
